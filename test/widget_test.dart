1ab4b0e4201b76acba71f4fb58c1d5e77d4c5850
Mohamed Nawar <nawar@example.com>
2026-08-06 09:02:58 +0300

    fix: remove unused material import to pass analyze

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:my_app/app/app.dart';
 import 'package:my_app/app/dependency_injection.dart';
